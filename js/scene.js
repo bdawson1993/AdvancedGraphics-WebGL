89980ad33acb8042a8755d7425520d12002c8c39
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2019-11-27 17:06:12 +0000

    latest changes

--- js/scene.js
+++ js/scene.js
@@ -10,7 +10,7 @@ console.log("Shader Loaded");
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,  0.1, 1000);
 var renderer = new THREE.WebGLRenderer({antilias: true});
-var gemometry = new THREE.BoxGeometry(1,1,1);
+var gemometry = new THREE.PlaneGeometry(10,10,10);
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 var material = new THREE.ShaderMaterial(
 {
@@ -29,16 +29,10 @@ function initScene()
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     controls = new THREE.OrbitControls( camera, renderer.domElement );
-    
-
-    for(var x = 0; x < 10; x++)
-    {
-        cube = new THREE.Mesh(gemometry, material);
-        cube.position.x = -5;
-        cube.position.x += 2 * x;
-        cubeArray.push(cube)
-        scene.add(cube);
-    }
+    cube = new THREE.Mesh(gemometry, material);
+    cubeArray.push(cube)
+    scene.add(cube);
+   
 
     camera.position.z = 5;
     addLighting();
@@ -58,11 +52,7 @@ function addLighting()
 
 function update()
 {
-    for(var x = 0; x < 10; x++)
-    {
-        cubeArray[x].rotation.x += 0.1;
-        cubeArray[x].rotation.z += 0.1;
-    }
+   
 
    
     renderer.render(scene, camera);
--- js/shaders/basic.frag
+++ js/shaders/basic.frag
@@ -5,6 +5,6 @@ void main()
 {
     vec3 light = vec3(0,1,0);
     float lambert = dot(normalize(fNormal), normalize(light));
-    vec3 ambient = vec3(0.1,0,0) * 0.5;
+    vec3 ambient = vec3(1,0,0) * 0.5;
     gl_FragColor = vec4(ambient + lambert * vec3(1,1,1), 1.0);
 }
